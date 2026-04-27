bb95b9ab4d083b74e8994d93e8ad6f9c9fa92635
sneh42raj <sneh.raj1517@gamil.com>
2026-04-27 14:17:13 +0530

    27-04-26

--- Strings/Countconsonents.cpp
+++ Strings/Countconsonents.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 int main(){
-
+    string s ;
+    cout<<"enter your string : ";
+    getline(cin , s) ;
+    int count = 0 ;
+    for(int i=0 ; i<s.size() ; i++){
+        if(s[i] != ('a') && s[i] != ('e') && s[i] != ('i') && s[i] != ('o') && s[i] != ('u') ) 
+        count ++ ;
+    }
 }    
 
