1779a3e6795f84594bfce707307ff133bc891e2c
sneh42raj <sneh.raj1517@gamil.com>
2026-06-10 14:01:02 +0530

    10-06-2026

---
+++ Sorting/Selection_insertion/InserstionSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include <algorithm>
+using namespace std;
+int main (){
+    int arr[]={5,3,1,4,2};
+    int n=5;
+    for(int i=0 ; i<n ;i++){
+       cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+    // inserstion sort
+    for(int i=1;i<n;i++){
+       int j = i ;
+    //    while(j>=1){
+    //     if(arr[j]>=arr[j-1]) break;
+    //     else swap(arr[j],arr[j-1]);
+    //     j--;
+    //    }
+        while(j>=1 && arr[j]<arr[j-1]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
+    for(int i=0 ; i<n ;i++){
+       cout<<arr[i]<<" ";
+    }
+}
