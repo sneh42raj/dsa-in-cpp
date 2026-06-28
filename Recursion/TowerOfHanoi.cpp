2edad8a667a73e242b75bdc5911ad766ffcdbe04
sneh42raj <sneh.raj1517@gamil.com>
2026-06-28 15:50:34 +0530

    28-06-2026

---
+++ Recursion/TowerOfHanoi.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void hanoi(int n, char s, char h, char d){
+    if(n==0) return;
+    hanoi(n-1,s,d,h);
+    cout<<s<<"->"<<d<<endl;
+    hanoi(n-1,h,s,d);
+}
+int main(){
+    int n;
+    cout<<"disks = ";
+    cin>>n;
+    hanoi(n,'A','B','c');
+
+}
